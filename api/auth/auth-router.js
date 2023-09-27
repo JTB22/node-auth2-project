7195099f804fa769894fa6636227dbaec4a541c2
Josh Blood <jblood92@pm.me>
2023-09-27 13:17:53 -0400

    MVP test pass

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
-const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
+const { checkUsernameExists, validateRoleName } = require("./auth-middleware");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const Users = require("../users/users-model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -14,8 +17,17 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
-});
+  const { username, password } = req.body;
+  const role_name = req.role_name;
 
+  const hash = bcrypt.hashSync(password, 10);
+  Users.add({ username, password: hash, role_name })
+    .then(([user]) => {
+      // console.log(user, username, role_name);
+      res.status(201).json({ user_id: user.user_id, username, role_name });
+    })
+    .catch(next);
+});
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
   /**
@@ -37,6 +49,31 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  const { username, password } = req.body;
+  const [user] = req.user;
+  if (!user || !password) {
+    res.status(401).json({ message: "Invalid credentials" });
+  } else {
+    const isValid = bcrypt.compareSync(password, user.password);
+    if (isValid) {
+      const token = generateJwt(user);
+      res.status(200).json({ message: `${username} is back!`, token });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  }
 });
+
+function generateJwt(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name,
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
 
 module.exports = router;
